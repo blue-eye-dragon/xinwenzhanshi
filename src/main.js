2cb9438a7f6b6b7ce78d7861e2ac717826a62cc5
lijenpeng <15831109557@139.com>
2020-02-05 16:53:19 +0800

    添加了路由模块

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+//导入路由
+// import router from './router/index.js'
+// router/index.js 可以省略index.js 默认访问index.js
+// ./ ../ 用这些方法去找对应的目录并不准确， 可以用vue.cli提供的——@来访问，相当于（src路径）
+//可以简写为以下的步骤
+import router from '@/router'
 
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  //对路由进行挂载
+  router
 }).$mount('#app')
