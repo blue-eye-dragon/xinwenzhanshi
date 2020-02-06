8a08a0b243d8165a804802fed9f86180a442931e
lijenpeng <15831109557@139.com>
2020-02-06 16:56:04 +0800

    表单验证完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
+
 import 'element-ui/lib/theme-chalk/index.css';
 //导入路由
 // import router from './router/index.js'
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,19 @@
   <div class='container-login'>
       <el-card class="my-card">
           <img src="../../assets/logo_index.png" alt="">
-          <el-form :model="form">
-              <el-form-item>
-                  <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
+          <el-form ref="form" :model="form" :rules="Rules">
+              <el-form-item prop="mobile">
+                  <el-input v-model="form.mobile" placeholder="请输入手机号" ></el-input>
               </el-form-item>
-              <el-form-item>
-                  <el-input v-model="form.code" placeholder="请输入验证码" style="width:240px;margin-right:8px"></el-input>
+              <el-form-item prop="code">
+                  <el-input v-model="form.code" placeholder="请输入验证码" style="width:240px;margin-right:8px" ></el-input>
                   <el-button>发送验证码</el-button>
               </el-form-item>
               <el-form-item>
                   <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
-              </el-form-item>
+              </el-form-item>   
                <el-form-item>
-                 <el-button type="primary" style="width:100%">登录</el-button>
+                 <el-button @click="login()" type="primary" style="width:100%">登录</el-button>
               </el-form-item>
           </el-form>
       </el-card>
@@ -27,12 +27,49 @@
 export default {
   name: 'Login',
   data() {
+      const checkmobile = (rule,value,callback) => {
+          if(!/^1[3-9]\d{9}$/.test(value)){
+              callback(new Error('手机号格式错误'))
+          }else{
+              callback()
+          }
+      }
       return {
           form:{
               mobile:'',
               code:'',
 
+          },
+          Rules:{
+              mobile:[
+                  {
+                      required:true,message:'请输入手机号',trigger:'blur'
+                  },
+                  {
+                      validator:checkmobile,trigger:'blur'
+                  }
+              ],
+              code:[
+                  {
+                      required:true,message:'请输入验证码',trigger:'blur'
+                  },
+                  {
+                      len:6,message:'验证码为6位数',trigger:'blur'
+                  }
+              ]
           }
+      }
+  },
+  methods: {
+      login(){
+          //对整体表单进行验证
+          //需要给整体的form元素添加ref属性，方便后续再vue中引用
+          this.$refs.form.validate((valid)=>{
+              //valid值为true时，验证成功
+              if(valid){
+                  
+              }
+          })
       }
   },
 }
