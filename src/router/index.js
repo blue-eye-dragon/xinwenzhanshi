015699481a68d919b9931acdc20ad891ca81be22
lijenpeng <15831109557@139.com>
2020-02-09 10:12:20 +0800

    侧边栏以及404页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,20 @@
  Vue.use(VueRouter)
  import Login from '@/views/login'
  import Home from '@/views/home'
+ import Welcome from '@/views/welcome'
+ import Notfound from '@/views/404'
  //初始化
  const router = new VueRouter({
      routes: [
         {path:'/login',component:Login},
-        {path:'/',component:Home}
+        {
+            path:'/',
+            component:Home,
+            children:[
+                {path:'/',component:Welcome}
+            ]
+        },
+        {path:'*',component:Notfound}
      ]
  })
 //导出
