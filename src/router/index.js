d765865c9ff82c439638a66a9a0d0f19547d0b9e
lijenpeng <15831109557@139.com>
2020-02-10 10:14:37 +0800

    artical

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+ //进行路由的初始化和注册
+ //导入
+ import VueRouter from 'vue-router'
+ //注册
+ import Vue from 'vue'
+ Vue.use(VueRouter)
+ import Login from '@/views/login'
+ import Home from '@/views/home'
+ import Welcome from '@/views/welcome'
+ import Notfound from '@/views/404'
+ import auth from '@/utils/auth'
+ import Artical from '@/views/artical'
+ //初始化
+ const router = new VueRouter({
+     routes: [
+        {path:'/login',component:Login},
+        {
+            path:'/',
+            component:Home,
+            children:[
+                {path:'/',component:Welcome},
+                {path:'/artical',component:Artical},
+            ]
+        },
+        {path:'*',component:Notfound}
+     ], 
+ })
+ router.beforeEach((to,from,next)=>{
+    if(to.path !== '/login' && !auth.getUser().token) return next('/login')
+    next()
+ })
+//导出
+export default router
