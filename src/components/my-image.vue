d36482a94fb689e0a688b2103a5f143e63e17c63
lijenpeng <15831109557@139.com>
2020-02-18 14:13:17 +0800

    完成文章发布和文章修改

--- src/components/my-image.vue
+++ src/components/my-image.vue
@@ -1,40 +1,71 @@
 <template>
   <div class="container-image">
     <div class="img_btn" @click="openDialog">
-      <img src="../assets/default.png" />
+      <img :src="value||imageBtn" />
     </div>
     <el-dialog :visible.sync="dialogVisible" width="750px">
       <span>
         <el-tabs v-model="activeName" type="card">
           <el-tab-pane label="素材库" name="list">
-            <!-- 按钮 -->
-            <el-radio-group v-model="reqParams.collect" size="small">
-              <el-radio-button :label="false">全部</el-radio-button>
-              <el-radio-button :label="true">收藏</el-radio-button>
-            </el-radio-group>
-            <!-- 图片列表 -->
-            <div class="img-list">
-              <div class="img-item" v-for="i in 8" :key="i">
-                <img src="../assets/avatar.jpg" alt />
+            <div v-loading="loading">
+              <!-- 按钮 -->
+              <el-radio-group @change="changeCollect" v-model="reqParams.collect" size="small">
+                <el-radio-button :label="false">全部</el-radio-button>
+                <el-radio-button :label="true">收藏</el-radio-button>
+              </el-radio-group>
+              <!-- 图片列表 -->
+              <div class="img-list">
+                <div
+                  @click="selectImage(item.url)"
+                  :class="{selected:selectedImage===item.url}"
+                  class="img-item"
+                  v-for="item in images"
+                  :key="item.id"
+                >
+                  <img :src="item.url" alt />
+                </div>
               </div>
+              <!-- 分页 -->
+              <el-pagination
+                background
+                layout="prev, pager, next"
+                hide-on-single-page
+                @current-change="changePage"
+                :current-page="reqParams.page"
+                :page-size="reqParams.per_page"
+                :total="total"
+              ></el-pagination>
             </div>
-            <!-- 分页 -->
-            <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
           </el-tab-pane>
-          <el-tab-pane label="上传图片" name="upload">配置管理</el-tab-pane>
+          <el-tab-pane label="上传图片" name="upload">
+            <el-upload
+              class="avatar-uploader"
+              action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+              name="image"
+              :headers="uploadHeader"
+              :show-file-list="false"
+              :on-success="handleSuccess"
+            >
+              <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-tab-pane>
         </el-tabs>
       </span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="configImage">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import auth from '@/utils/auth'
+import defaultImage from '@/assets/default.png'
 export default {
   name: "my-image",
+  props: ['value'],
   data() {
     return {
       reqParams: {
@@ -42,13 +73,63 @@ export default {
         page: 1,
         per_page: 8
       },
+      loading: false,
       dialogVisible: false,
-      activeName: "list"
+      activeName: "list",
+      images: [],
+      total: 0,
+      selectedImage: null,
+      uploadHeader: {
+        Authorization: `Bearer ${auth.getUser().token}`
+      },
+      imageUrl: null,
+      imageBtn: defaultImage
     };
   },
   methods: {
+    configImage(){
+      if(this.activeName === 'list'){
+        if(!this.selectedImage) return this.$message.warning('请从素材中选择一张图片')
+        // this.imageBtn = this.selectedImage
+        this.$emit('input',this.selectedImage)
+      }else{
+        if(!this.imageUrl) return this.$message.warning('请上传一张图片')
+        // this.imageBtn = this.imageUrl
+        this.$emit('input',this.imageUrl)
+      }
+      this.dialogVisible = false
+    },
+    handleSuccess(res){
+      this.imageUrl = res.data.url
+      this.$message.success('上传成功')
+    },
+    selectImage(url) {
+      this.selectedImage = url;
+    },
+    changePage(newpage) {
+      this.reqParams.page = newpage;
+      this.getImage();
+    },
+    changeCollect() {
+      this.reqParams.page = 1;
+      this.getImage();
+    },
     openDialog() {
       this.dialogVisible = true;
+      this.getImage();
+      this.activeName = 'list';
+      this.selectedImage = null;
+      this.imageUrl = null
+    },
+    async getImage() {
+      this.loading = true;
+      const res = await this.$http.get("user/images", {
+        params: this.reqParams
+      });
+      this.loading = false;
+      this.images = res.data.data.results;
+      console.log(this.images);
+      this.total = res.data.data.total_count;
     }
   }
 };
@@ -63,10 +144,27 @@ export default {
     border: 1px dashed #ddd;
     display: inline-block;
     margin-right: 20px;
+    position: relative;
     img {
       width: 100%;
       height: 100%;
       display: block;
+    }
+    &.selected::after {
+      // .img-item 的后伪元素
+      // &符号 less语法，连接符，连接 上一级选择和当前的选择器。
+      // 此时不加&：.img-item ::after{}  解析后：后代选择器
+      // 加上&符后：.img-item::after{}   解析后：交集选择器
+      // .img-item.selected::after{} 满足这个选择器样式生效
+      // 当你想选中效果：给.img-item加上selected类即可。
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.3) url(../assets/selected.png) no-repeat
+        center/ 50px auto;
     }
   }
 }
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container-publish">
     <el-card>
-      <div slot="hearer">
-        <my-bread>发布文章</my-bread>
+      <div slot="header">
+        <my-bread>{{$route.query.id?'修改':'发布'}}文章</my-bread>
       </div>
       <el-form label-width="120px">
         <el-form-item label="标题：">
@@ -12,24 +12,28 @@
           <quill-editor v-model="articalForm.content" :options="editorOption"></quill-editor>
         </el-form-item>
         <el-form-item label="封面：">
-          <el-radio-group v-model="articalForm.cover.type">
+          <el-radio-group @change="articalForm.cover.images=[]" v-model="articalForm.cover.type">
             <el-radio :label="1">单图</el-radio>
             <el-radio :label="3">三图</el-radio>
             <el-radio :label="0">无图</el-radio>
             <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
-          <div>
-            <my-image></my-image>
-            <my-image></my-image>
-            <my-image></my-image>
+          <div v-if="articalForm.cover.type === 1">
+            <my-image v-model="articalForm.cover.images[0]"></my-image>
+          </div>
+          <div v-if="articalForm.cover.type === 3">
+            <my-image :key="i" v-for="i in 3" v-model="articalForm.cover.images[i-1]"></my-image>
           </div>
         </el-form-item>
         <el-form-item label="频道：">
           <my-channel v-model="articalForm.channel_id"></my-channel>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary">发布文章</el-button>
-          <el-button>存入草稿</el-button>
+        <el-form-item v-if="$route.query.id">
+          <el-button @click="updata()" type="success">修改文章</el-button>
+        </el-form-item>
+        <el-form-item v-else>
+          <el-button @click="submit(false)" type="primary">发布文章</el-button>
+          <el-button @click="submit(true)">存入草稿</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -49,7 +53,7 @@ export default {
   data() {
     return {
       articalForm: {
-        title: "",
+        title: null,
         channel_id: null,
         content: null,
         cover: {
@@ -71,6 +75,62 @@ export default {
         }
       }
     };
+  },
+  created() {
+    if (this.$route.query.id) {
+      this.getArticle();
+    }
+  },
+  watch: {
+    "$route.query.id": function() {
+      this.toggleFormInfo();
+    }
+  },
+  methods: {
+    async updata() {
+      try {
+        await this.$http.put(
+          `articles/${this.articalForm.id}?draft=false`,
+          this.articalForm
+        );
+        this.$message.success("修改文章成功");
+        this.$router.push("/artical");
+      } catch (e) {
+        this.$message.error("修改文章失败");
+      }
+    },
+    async getArticle() {
+      const res = await this.$http.get(`articles/${this.$route.query.id}`);
+      this.articalForm = res.data.data;
+    },
+    toggleFormInfo() {
+      if (this.$route.query.id) {
+        this.getArticle();
+      } else {
+        this.articalForm = {
+          title: null,
+          channel_id: null,
+          content: null,
+          cover: {
+            type: 1,
+            images: []
+          }
+        };
+      }
+    },
+    // 添加文章 draft===false 发布文章  draft===true  存入草稿
+    async submit(draft) {
+      try {
+        // 请求
+        await this.$http.post(`articles?draft=${draft}`, this.articalForm);
+        // 提示
+        this.$message.success(draft ? "存入草稿成功" : "发布文章成功");
+        // 跳转
+        this.$router.push("/artical");
+      } catch (e) {
+        this.$message.error("操作失败");
+      }
+    }
   }
 };
 </script>
